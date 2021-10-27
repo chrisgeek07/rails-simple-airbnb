cd7ca2141a6bc1de82d25df70366a74f67d20cb1
Christian BeauxYeux <gildweek@gmail.com>
2021-10-27 17:04:28 +0400

    Basic card setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  Flat.create!(
+    name: Faker::Demographic.race,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(40..300),
+    number_of_guests: rand(1..6)
+  )
+end
+
+puts 'All Flats have been created'
