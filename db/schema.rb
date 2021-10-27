7a08fbfa62f5741b137bc83bbac4fa4d37da5749
Christian BeauxYeux <gildweek@gmail.com>
2021-10-27 22:02:25 +0400

    All logic created with basic CSS styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_125302) do
-
-  create_table "flats", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.text "description"
-    t.integer "price_per_night"
-    t.integer "number_of_guests"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_10_27_171834) do
+  create_table 'flats', force: :cascade do |t|
+    t.string 'name'
+    t.string 'address'
+    t.text 'description'
+    t.integer 'price_per_night'
+    t.integer 'number_of_guests'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.string 'picture_url'
   end
-
 end
